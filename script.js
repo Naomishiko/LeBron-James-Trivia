ada5fbc97df101880abe7543c95c382a7562a096
njivoon@gmail.com <njivoon@gmail.com>
2022-03-15 17:00:55 -0700

    LBJTrivia

---
+++ script.js
@@ -0,0 +1,533 @@
+
+const Questions = [{
+  id: 0,
+  q: "Who is LeBron James?",
+  a: [{ text: "American Basketball Player", isRight: true },
+      { text: "American Tennis Player", isRight: false },
+      { text: "American Volleyball Player", isRight: false },
+      { text: "American Baseball Player", isRight: false }
+  ]
+
+
+
+},
+{
+  id: 1,
+  q: "How old is LeBron James?",
+  a: [{ text: "25", isRight: false },
+      { text: "40", isRight: false },
+      { text: "28", isRight: false },
+      { text: "37", isRight: true }
+  ]
+
+
+
+
+},
+{
+  id: 3,
+  q: "What is the name of LeBron James wife?",
+  a: [{ text: "Gabrielle Union", isRight: false},
+      { text: "Savannah James", isRight: true},
+      { text: "Kelly James", isRight: false},
+      {text: "Samantha James", isRight: false}
+]
+
+
+
+
+},
+{
+  id: 5,
+  q: "How many kids LeBron James have?",
+  a: [{ text: "6", isRight: false},
+      { text: "5", isRight: false},
+      { text: "4", isRight: false},
+      { text: "3", isRight: true},
+]
+},
+
+
+
+{ 
+  id: 6,
+  q: "How many championships does LeBron James have?",
+  a: [{ text: "8", isRight: false},
+      { text: "6", isRight: false},
+      { text: "4", isRight: true},
+      { text: "2", isRight: false},
+]
+},
+
+
+{
+  id: 7,
+  q: "For which NBA team did LeBron James play his rookie season?",
+  a: [{ text: "Miami Heat", isRight: false},
+      { text: "New York Knicks", isRight: false},
+      { text: "Boston Celtics", isRight: false},
+      { text: "Cleveland Cavaliers", isRight: true},
+]
+},
+
+
+{
+  id: 8,
+  q: "What high school sport did LeBron James Play along with basketball?",
+  a: [{ text: "Soccer", isRight: false},
+      { text: "Baseball", isRight: false},
+      { text: "Football", isRight: true},
+      { text: "Hockey", isRight: false},
+]
+},
+
+
+
+{
+  id: 9,
+  q: "What is the name of LeBron James mother?",
+  a: [{ text: "Mary", isRight: false},
+      { text: "Joan", isRight: false},
+      { text: "Gloria", isRight: true},
+      { text: "Jane", isRight: false},
+]
+},
+
+
+{
+  id: 10,
+  q: "In what city, did LeBron James and Savannah Brinson get married?",
+  a: [{ text: "Miami beach", isRight: false},
+      { text: "Akron", isRight: false},
+      { text: "San Diego", isRight: true},
+      { text: "Los Angeles", isRight: false},
+]
+},
+
+
+{
+  id: 11,
+  q: "LeBron James signed with what company for $90 million?",
+  a: [{ text: "Nike", isRight: true},
+      { text: "Adidas", isRight: false},
+      { text: "Puma", isRight: false},
+      { text: "Reebok", isRight: false},
+]
+},
+
+
+
+
+{
+  id: 12,
+  q: "Which charity does LeBron James support?",
+  a: [{ text: "Love", isRight: false},
+      { text: "Children's Defense Fund", isRight: true},
+      { text: "Children's Basketball", isRight: false},
+      { text: "Capital", isRight: false},
+]
+},
+
+
+
+{ 
+  id: 13,
+  q: "LeBron James high scoring game in 2004-2005 came against Toronto; how many points did he score?",
+  a: [{ text: "33 Points", isRight: false},
+      { text: "56 Points", isRight: true},
+      { text: "45 Points", isRight: false},
+      { text: "60 Points", isRight: false},
+]
+},
+
+
+{
+  id: 14,
+  q: "LeBron James was the youngest player in league history to be awarded which of the following?",
+  a: [{ text: "ALL-star Honors", isRight: false},
+      { text: "NBA star Honors", isRight: false},
+      { text: "All-NBA Honors", isRight: true},
+      { Text: "Western Honors", isRight: false},
+]
+},
+
+
+{
+  id: 15,
+  q: "What is the name of one of LeBron James's son?",
+  a: [{ text: "Mike", isRight: false},
+      { text: "John", isRight: false},
+      { text: "Paul", isRight: false},
+      { text: "Bronny", isRight: true},
+]
+},
+
+
+
+{
+  id: 16,
+  q: "What state is LeBron from?",
+  a: [{ text: "Ohio", isRight: true},
+      { text: "California", isRight: false},
+      { text: "Miami", isRight: false},
+      { text: "Arizona", isRight: false},
+]
+},
+
+
+
+{
+  id: 17,
+  q: "Who Drafted LeBron?",
+  a: [{ text: "LA Lakers", isRight: false},
+      { text: "Miami Heat", isRight: false},
+      { text: "Phoenix Suns", isRight: false},
+      { text: "Cleveland Cavaliers", isRight: true},
+]
+},
+
+
+
+{
+  id: 18,
+  q: "What college did LeBron go to?",
+  a: [{ text: "Didn't go to college", isRight: true},
+      { text: "Duke", isRight: false},
+      { text: "Ohio State", isRight: false},
+      { text: "Arizona State", isRight: false},
+]
+},
+
+
+
+
+{ 
+  id: 19,
+  q: "What is LeBron James number for the LA Lakers?",
+  a: [{ text: "19", isRight: false},
+      { text: "23", isRight: true},
+      { text: "24", isRight: false},
+      { text: "26", isRight: false},
+]
+},
+
+
+
+{
+  id: 20,
+  q: "Which team did LeBron win his first NBA Championship with?",
+  a: [{ text: "Miami Heat", isRight: true},
+      { text: "Orlando Magic", isRight: false},
+      { text: "Indiana Pacers", isRight: false},
+      { text: "Cleveland Cavaliers", isRight: false},
+]
+},
+
+
+
+{
+  id: 21,
+  q: "What is the current nickname of LeBron?",
+  a: [{ text: "Speed", isRight: false},
+      { text: "Shooter", isRight: false},
+      { text: "Jumper", isRight: false},
+      { text: "King James", isRight: true},
+]
+},
+
+
+
+{
+  id: 22,
+  q: "Which team did LeBron beat when leading a 3-1 comeback in the NBA finals?",
+  a: [{ text: "Golden State Warriors", isRight: true},
+      { text: "Dallas Mavericks", isRight: false},
+      { text: "Brooklyn Nets", isRight: false},
+      { text: "Toronto Raptors", isRight: false},
+]
+},
+
+
+{
+  id: 23,
+  q: "Which spot was LeBron drafted in his draft class?",
+  a: [{ text: "4th", isRight: false},
+      { text: "2nd", isRight: false},
+      { text: "1st", isRight: true},
+      { text: "6th", isRight: false},
+]
+},
+
+
+
+{
+  id: 24,
+  q: "How many regular season MVP Awards does LeBron have?",
+  a: [{ text: "4", isRight: true},
+      { text: "2", isRight: false},
+      { text: "5", isRight: false},
+      { text: "7", isRight: false},
+]
+},
+
+
+
+
+{
+  id: 24,
+  q: "How tall is LeBron?",
+  a: [{ text: "6'1", isRight: false},
+      { text: "6'4", isRight: false},
+      { text: "6'8", isRight: true},
+      { text: "7'1", isRight: false},
+]
+},
+
+
+{
+  id: 25,
+  q: "How many NBA Finals MVP Awards does LeBron have?",
+  a: [{ text: "8", isRight: false},
+      { text: "6", isRight: false},
+      { text: "2", isRight: false},
+      { text: "4", isRight: true},
+]
+},
+
+
+
+{
+  id: 26,
+  q: "Which shoe line does LeBron endorse?",
+  a: [{ text: "Puma", isRight: false},
+      { text: "Nike", isRight: true},
+      { text: "Under Armour", isRight: false},
+      { text: "Adidas", isRight: false},
+]
+},
+
+
+
+{
+  id: 26,
+  q: "What number did LeBron wear to start his NBA career?",
+  a: [{ text: "14", isRight: false},
+      { text: "24", isRight: false},
+      { text: "23", isRight: true},
+      { text: "6", isRight: false},
+]
+},
+
+
+{
+  id: 27,
+  q: "Which year did LeBron get drafted into the NBA?",
+  a: [{ text: "2003", isRight: true},
+      { text: "2005", isRight: false},
+      { text: "2008", isRight: false},
+      { text: "2001", isRight: false},
+  ]
+},
+
+
+{
+  id: 28,
+  q: "Which team did LeBron beat to win his first NBA Championship?",
+  a: [{ text: "San Antonio Spurs", isRight: false},
+      { text: "Boston Celtics", isRight: false},
+      { text: "Golden State Warriors", isRight: false},
+      { text: "Oklahoma City Thunder", isRight: true},
+]
+},
+
+
+{
+  id: 29,
+  q: "What was the high school nickname of LeBron?",
+  a: [{ text: "Bronny", isRight: false},
+      { text: "The Chosen One", isRight: true},
+      { text: "The Beast", isRight: false},
+      { text: "Shooter", isRight: false},
+]
+},
+
+
+{
+  id: 30,
+  q: "How many Olympics gold medals does LeBron have?",
+  a: [{ text: "3", isRight: false},
+      { text: "2", isRight: true},
+      { text: "6", isRight: false},
+      { text: "4", isRight: false},
+]
+},
+
+
+{
+  id: 32,
+  q: "Who was LeBron's coach on the Miami Heat?",
+  a: [{ text: "Ty Lue", isRight: false},
+      { text: "Pat Riley", isRight: false},
+      { text: "Steve Kerr", isRight: false},
+      { text: "Erik Spoelstra", isRight: true},
+]
+},
+
+
+{
+  id: 33,
+  q: "What is LeBron's career high in points scored in a single game?",
+  a: [{ text: "61", isRight: true},
+      { text: "60", isRight: false},
+      { text: "48", isRight: false},
+      { text: "58", isRight: false},
+]
+},
+
+
+{
+  id: 34,
+  q: "What is the name of LeBron's former high school team?",
+  a: [{ text: "Hamilton High School", isRight: false},
+      { text: "Ohio High School", isRight: false},
+      { text: "Luke High School", isRight: false},
+      { text: "St. Vincent-St. Mary", isRight: true},
+]
+},
+
+
+{
+  id: 35,
+  q: "What is LeBron James middle name?",
+  a: [{ text: "Bronny", isRight: false},
+      { text: "Anthony", isRight: false},
+      { text: "Mike", isRight: false},
+      { text: "Raymone", isRight: true},
+]
+},
+
+
+
+
+]
+var start = true;
+
+// Iterate
+function iterate(id) {
+
+  // Getting the result display section
+  var result = document.getElementsByClassName("result");
+  result[0].innerText = "";
+
+  // Getting the question
+  const question = document.getElementById("question");
+
+
+  // Setting the question
+  question.innerText = Questions[id].q;
+// Getting the options
+const op1 = document.getElementById('op1');
+const op2 = document.getElementById('op2');
+const op3 = document.getElementById('op3');
+const op4 = document.getElementById('op4');
+
+
+// Providing option text 
+op1.innerText = Questions[id].a[0].text;
+op2.innerText = Questions[id].a[1].text;
+op3.innerText = Questions[id].a[2].text;
+op4.innerText = Questions[id].a[3].text;
+
+
+
+
+// True or false value for the options
+op1.value = Questions[id].a[0].isRight;
+op2.value = Questions[id].a[1].isRight;
+op3.value = Questions[id].a[2].isRight;
+op4.value = Questions[id].a[3].isRight;
+
+
+
+
+
+
+
+var selected = "";
+
+// Selected for op1
+op1.addEventListener("click", () => {
+    op1.style.backgroundColor = "yellow";
+    op2.style.backgroundColor = "pink";
+    op3.style.backgroundColor = "pink";
+    op4.style.backgroundColor = "pink";
+    
+
+    selected = op1.value;
+})
+
+// Selected for op2
+op2.addEventListener("click", () => {
+    op1.style.backgroundColor = "pink";
+    op2.style.backgroundColor = "yellow";
+    op3.style.backgroundColor = "pink";
+    op4.style.backgroundColor = "pink";
+   
+    selected = op2.value;
+})
+
+// Selected for op3
+op3.addEventListener("click", () => {
+  op1.style.backgroundColor = "pink";
+  op2.style.backgroundColor = "pink";
+  op3.style.backgroundColor = "yellow";
+  op4.style.backgroundColor = "pink";
+ 
+
+
+  selected = op3.value;
+})
+
+// Selected for op4
+op4.addEventListener("click", () => {
+  op1.style.backgroundColor = "pink";
+  op2.style.backgroundColor = "pink";
+  op3.style.backgroundColor = "pink";
+  op4.style.backgroundColor = "yellow";
+ 
+
+  selected = op4.value;
+})
+
+// Grabbing submit
+const submit = document.getElementsByClassName("submit");
+
+// submit
+submit[0].addEventListener("click", () => {
+   if (selected == "true") {
+       result[0].innerHTML = "True";
+       result[0].style.color = "green";
+   } else {
+       result[0].innerHTML = "False";
+       result[0].style.color = "red";
+   }
+})
+}
+
+if (start) {
+iterate("0");
+}
+
+// Next question button and method
+const nextquestion = document.getElementsByClassName('nextquestion')[0];
+var id = 0;
+
+nextquestion.addEventListener("click", () => {
+  start = false;
+  if (id < 35) {
+      id++;
+      iterate(id);
+      console.log(id);
+  }
+
+})
